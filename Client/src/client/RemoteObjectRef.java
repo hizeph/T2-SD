5787ec5f4fab9712fc031cf839c86fbea143df7b
Cezar <cezar.a.bernardi@gmail.com>
2014-04-26 17:44:55 -0300

    starting

---
+++ Client/src/client/RemoteObjectRef.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client;
+
+import java.net.InetAddress;
+
+/**
+ *
+ * @author Hizeph
+ */
+public class RemoteObjectRef {
+    
+    private InetAddress ip;
+    private int port;
+    private double time;
+    private int objectNumber;
+    //Interface of remote object - wtf?
+    
+}
